e116a66e0a3c6ab155b26e601fd7d89d0b3332af
olga bachinskaya <myvarvara2211@gmail.com>
2018-08-22 23:24:07 +0300

    add section-price and slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import $ from 'jquery';
 
 
+// slider
+let slides = document.querySelectorAll('#slides .slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 4000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+//   /slider
