e2a4647050332fc0a99711c72740df0baaad24a6
olga bachinskaya <myvarvara2211@gmail.com>
2018-08-25 19:25:36 +0300

    add map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import $ from 'jquery';
-
-
 // slider
 let slides = document.querySelectorAll('#slides .slide');
 let currentSlide = 0;
@@ -12,3 +10,28 @@ function nextSlide() {
     slides[currentSlide].className = 'slide showing';
 }
 //   /slider
+
+// footer
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    let mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(50.4760069, 30.4075262), // New York
+        styles: [{
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{
+                "visibility": "on"
+            }]
+        }]
+    };
+    let mapElement = document.getElementById('map');
+    let map = new google.maps.Map(mapElement, mapOptions);
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(50.4760069, 30.4075262),
+        map: map,
+        title: 'Snazzy!'
+    });
+}
+// /footer
